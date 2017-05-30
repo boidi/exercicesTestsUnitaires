0583b9c75431cf5d44a3bf08b3d7339af9ba3db1
jpdenet <jdenet@simplon.co>
2017-05-30 14:04:34 +0200

    exercices tests unitaires

---
+++ src/questions.js
@@ -0,0 +1,65 @@
+/**
+ *
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+var tailleString = function (texte) {
+    return //à compléter ;
+}
+var remplaceECar = function (texte) {
+    return //à compléter ;
+}
+var concatString = function (texte1, texte2) {
+    var multitext = texte1 + texte2;
+    return //à compléter ;
+}
+var afficherCar5 = function (texte) {
+    return //à compléter ;
+}
+var afficher9Car = function (texte) {
+    return //à compléter ;
+}
+var majusculeString = function (texte) {
+    return //à compléter ;
+}
+var minusculeString = function (texte) {
+    return //à compléter ;
+}
+var SupprEspaceString = function (texte) {
+    return //à compléter ;
+}
+var IsString = function (texte) {
+    return //à compléter ;
+}
+var AfficherExtensionString = function (texte) {
+    return //à compléter ;
+}
+var NombreEspaceString = function (texte) {
+    return //à compléter ;
+}
+var InverseString = function (texte) {
+    return //à compléter ;
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+var calculPuissance = function (x, y) {
+    return //à compléter ;
+}
+var valeurAbsolue = function (nombre) {
+    return //à compléter ;
+}
+var valeurAbsolueArray = function (array) {
+    return //à compléter ;
+}
+var sufaceCercle = function (rayon) {
+    return //à compléter ;
+}
+var hypothenuse = function (ab, ac) {
+    return //à compléter ;
+}
+var calculIMC = function (poids, taille) {
+    return ;
+}
